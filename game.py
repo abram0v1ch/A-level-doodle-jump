a7c63518938d185cf9db84a704657d4021ae754d
Vasyl <abramovich.vasyl@gmail.com>
2021-01-27 10:20:15 +0000

    'second'

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 #-- Global constants
 
 #-- Colors
@@ -10,6 +11,10 @@ YELLOW = (255,255,0)
 #-- Initialise pygame and clock
 pygame.init()
 
+#variables needed for classes
+prev = None
+same = False
+
 class Player(pygame.sprite.Sprite):
 	def __init__(self):
 		super().__init__()
@@ -18,10 +23,10 @@ class Player(pygame.sprite.Sprite):
 		#set the position of the sprite
 		self.rect = self.image.get_rect()
 		self.rect.x = 260
-		self.rect.y = 760
-		self.speed = 66
+		self.rect.y = 720
+		self.speed = 48
 
-	def update(self, change):
+	def update(self, change, platforms):
 		self.rect.y -= self.speed/3
 		if self.speed > 0:
 			self.speed -= 3
@@ -29,13 +34,31 @@ class Player(pygame.sprite.Sprite):
 			self.speed = -33
 		if self.speed < 0:
 			self.speed += 3
-		if self.rect.y >= 760:
-			self.speed = 66
+		if self.speed < 0 and pygame.sprite.spritecollideany(self, platforms):
+			self.speed = 48
 		self.rect.x += change
 		if self.rect.x == -40:
 			self.rect.x = 540
 		if self.rect.x == 550:
 			self.rect.x = -30
+
+
+class Platform(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		super().__init__()
+		self.image = pygame.Surface([60, 10])
+		self.image.fill(BLUE)
+		#set the position of the sprite
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+
+	def move(self, player):
+		if player.speed > 0:
+			self.rect.y += player.speed/3
+		if self.rect.y >= 800:
+			self.kill()
+
 
 #-- Blank screen
 size = (550,800)
@@ -53,6 +76,7 @@ clock = pygame.time.Clock()
 
 player = Player()
 group = pygame.sprite.Group()
+platforms = pygame.sprite.Group()
 group.add(player)
 
 ###-- Game Loop
@@ -63,9 +87,9 @@ while not done:
             exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                change = -5
+                change = -10
             elif event.key == pygame.K_RIGHT:
-                change = 5
+                change = 10
              #endif
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
@@ -73,16 +97,40 @@ while not done:
             #endif
         #endif
     # Game logic goes after this comment
+    max_h = 800
+    while len(platforms) < 7:
+    	if len(platforms) > 0:
+    		for x in platforms:
+    			if x.rect.y < max_h:
+    				max_h = x.rect.y
+    		platforms.add(Platform(random.randint(0, 490), max_h-110))
+    	else:
+    		platforms.add(Platform(240, 760))
 
     # Screen background is BLACK
     screen.fill(BLACK)
 
+    platforms.draw(screen)
     group.draw(screen)
-    player.update(change)
+    player.update(change, platforms)
+    if prev != None:
+    	if pygame.sprite.collide_rect(player, prev):
+    		same = True
+    		print(same)
+
+    for x in platforms:
+    	if pygame.sprite.collide_rect(player, x):
+    		prev = x
+
+    if same == False:
+    	for x in platforms:
+    		x.move(player)
 
     #--flip display to reveal new position of objects
     pygame.display.flip()
     clock.tick(60)
+
+    same = False
 
 ###--End of game loop
 pygame.quit()
